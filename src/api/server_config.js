1c73ecf377f89568a0e47bb849ea23e0b8ae15a9
Equent <ryq2836@qq.com>
2022-01-21 13:39:06 +0800

    0.6beta

--- src/api/server_config.js
+++ src/api/server_config.js
@@ -1,6 +1,6 @@
 export default {
   // fileServer: 'http://localhost:3007',  //dev
-  // shareServer: 'http://localhost:3007', //dev
+  shareServer: 'http://localhost:3007', //dev
   // fileServer: 'http://localhost:3007',  //proc
-  shareServer: 'http://120.25.222.200:3007', //proc
+  // shareServer: 'http://120.25.222.200:3007', //proc
 }
